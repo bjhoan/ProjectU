04c8f9fae3587f1c1f1fd605f104bd795f1a3182
bjhoan <jstevenpiconc@gmail.com>
2024-02-11 14:02:00 -0500

    Update Home.jsx

--- ProjectU/src/pages/Home.jsx
+++ ProjectU/src/pages/Home.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div><h1>Home</h1></div>
+    <div>
+      <h1>Home</h1>
+      <p>Text</p>
+      <hr />
+      <h2>Sub</h2>
+      <p>Text</p>
+      <hr />
+    </div>
   )
 }
 
